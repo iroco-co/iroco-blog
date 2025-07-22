229953c9e2e5039f9c7b0a5dbdd7820cf20ebfeb
Bruno Thomas <bruno@barreverte.fr>
2025-07-22 14:13:58 +0200

    add: nouvel article sur haproxy/acme

--- jekyll_docker.sh
+++ jekyll_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # -*- coding: UTF8 -*-
 
-docker run --rm --volume=$(pwd):/srv/jekyll -it -p 127.0.0.1:4000:4000 jekyll/jekyll:4 bundle install && jekyll serve --drafts --watch
+docker run --rm --volume=$(pwd):/srv/jekyll -it -p 127.0.0.1:4000:4000 jekyll/jekyll:4 apk add make gcc musl-dev && bundle install && bundle exec jekyll serve --drafts --watch
