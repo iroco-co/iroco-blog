b7046c28af71a5d82ab37957e2960fc449c2e4af
Bruno Thomas <bruno@barreverte.fr>
2025-10-20 15:15:24 +0200

    fix: remove rm that removes cached container

--- jekyll_docker.sh
+++ jekyll_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # -*- coding: UTF8 -*-
 
-docker run --rm --volume="$(pwd):/srv/jekyll" -it -p 127.0.0.1:4000:4000 jekyll/jekyll:4 \
+docker run --volume="$(pwd):/srv/jekyll" -it -p 127.0.0.1:4000:4000 jekyll/jekyll:4 \
   sh -c "apk add make gcc musl-dev && bundle install && bundle exec jekyll serve --drafts --watch --host 0.0.0.0"
